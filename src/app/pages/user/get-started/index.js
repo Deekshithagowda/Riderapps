ed67a077cd7be34e0faaf4ca75f1947a21917265
rahulkumar-aditri <rahul.kumar@aditri.com>
2025-10-16 10:38:48 +0530

    fixed image library code

--- src/app/pages/user/get-started/index.js
+++ src/app/pages/user/get-started/index.js
@@ -2,7 +2,6 @@
 
 
 import { Text, View } from "react-native";
-import FastImage from "react-native-fast-image";
 import { responsiveWidth as rw, responsiveHeight as rh } from "react-native-responsive-dimensions";
 import { moderateScale as ms } from "react-native-size-matters";
 
@@ -12,7 +11,6 @@ export default function GetStarted() {
 
     return (
         <View style={{ width: rw(100) }} className='flex-1 bg-primary items-center justify-center'>
-             <FastImage source={{uri:'https://images.pexels.com/photos/33806640/pexels-photo-33806640.jpeg'}} resizeMode='cover' style={{width:300,height:300}}/>
 
             <Text style={{ fontSize: ms(14) }}>Hello this is getstarted page</Text>
         </View>
---
+++ src/app/utils/responsive/dimensions.js
@@ -0,0 +1,10 @@
+
+
+import {
+    responsiveWidth,
+    responsiveHeight,
+} from "react-native-responsive-dimensions";
+import { moderateScale } from "react-native-size-matters";
+
+export const rw = (val) => responsiveWidth(val);
+export const rh = (val) => responsiveHeight(val);
